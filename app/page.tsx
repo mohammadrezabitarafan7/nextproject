7d21a08793d0c314cc5ac105b065c6f2d29b6b1d
mohammadreza <mohammadrezabitarafan7@gmail.com>
2024-01-16 16:17:45 +0330

    1

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ import Image from 'next/image';
 
 
 export default function Page() {
+
+  console.log("hello");
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
